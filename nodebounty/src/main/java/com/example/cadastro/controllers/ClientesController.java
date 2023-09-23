b2f9f653687d3d557c9fd6ecfd019c575150ab1e
Eloah Baracho <eloahbarachodossantos@gmail.com>
2023-09-23 13:21:54 -0300

    migrations, flyway e criação do banco

---
+++ nodebounty/src/main/java/com/example/cadastro/controllers/ClientesController.java
@@ -0,0 +1,30 @@
+package com.example.cadastro.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Clientes") 
+
+/* 
+ * RequestMapping mostra por qual endpoint esse controller é responsável.
+ * RestController define um controller
+  */ 
+ 
+public class ClientesController {
+
+	@GetMapping 
+	
+	/* GetMapping Indica que esse método abaixo é responsável por cuidar das requisições
+que baterem no /Clientes e sejam do método get
+	*/ 
+	
+	public ResponseEntity GetAllClientes() {
+		return ResponseEntity.ok("Deu ok!");
+		/* ResponseEntity é o tipo de classe utilizada para a manipulação
+		 * de dados retornados
+		 */
+	}
+}
