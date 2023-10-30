5f8ad7828c5f04f11fb5d68c573a4d09ac35388f
DarkMetaK <matporto03@gmail.com>
2023-10-30 20:41:12 -0300

    updates

--- nodebounty/src/main/java/com/nodebounty/domain/cliente/DadosAtualizacaoCliente.java
+++ nodebounty/src/main/java/com/nodebounty/domain/cliente/DadosAtualizacaoCliente.java
@@ -1,14 +1,11 @@
 package com.nodebounty.domain.cliente;
 
-import jakarta.validation.constraints.NotBlank;
-
 /* O ID é o único campo obrigatório. Todos os outros serão opcionais. Algumas coisas em teoria não pode mudar
  * Então nem insiro eles aqui, pois não são uma opção. Na minha concepção seria RG, CPF, data de nascimento 
  * e e-mail pra evitar uma chatice depois
  */
 
 public record DadosAtualizacaoCliente(
-		@NotBlank(message="Esse campo é obrigatório") String idCliente,
 		String nome, 
 		String endereco,
 		String cep,
